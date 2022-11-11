517c29d6073bfe112823c4692db3d9f62ac7e6be
Wallyson <wallyson@aluno.fapce.edu.br>
2022-11-11 20:56:35 -0300

    feature/[MDL02-01] - How to start -i mode

---
+++ MDL02/interactive_mode/how_to_start_I_Mode.py
@@ -0,0 +1,8 @@
+# For execute a interactive mode in python you can run the script using flag -i
+# example: python -i app.py
+
+#Dir function whitout args return a name list of local scope currenty
+# With Args return one list of valid attributes for the object. Example: dir()/dir(100)
+
+## Help function invoke a system of helper interagrate. Is possible make searchs em interative mode or pass the param with the name, module function, class, method, or variable. 
+# example: help() / help(100)
