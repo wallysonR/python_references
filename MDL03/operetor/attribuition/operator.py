fdf2eaae70eb27484d878b26ceb49ad76e70e239
Wallyson <wallyson@aluno.fapce.edu.br>
2022-11-12 17:40:26 -0300

    feature/[MDL01-03] - operator attribuition

---
+++ MDL03/operetor/attribuition/operator.py
@@ -0,0 +1,25 @@
+saldo = 500 # atribuindo 500 para varivel saldo
+print(saldo)
+
+saldo = 500 # atribuindo 500 para varivel saldo
+saldo += 500 # atribuição com adiciononal
+print(saldo)
+saldo = 500 # atribuindo 500 para varivel saldo
+saldo -= 500# atribuicao com subtracao
+print(saldo)
+saldo = 500 # atribuindo 500 para varivel saldo
+saldo *= 500#atribuicao com multiplicacao
+print(saldo)
+saldo = 500 # atribuindo 500 para varivel saldo
+saldo /= 500# atribuicao com divisao
+print(saldo)
+saldo = 500 # atribuindo 500 para varivel saldo
+saldo //= 500# atribuicao com divisao inteira
+print(saldo)
+
+saldo = 500 # atribuindo 500 para varivel saldo
+saldo %= 480 #atribuicao com modulo
+print(saldo)
+saldo = 500 # atribuindo 500 para varivel saldo
+saldo **= 2 # atribuicao com exponenciacao
+print(saldo)
