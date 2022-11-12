8cfe3728adefd6740be623dd42f1e3dd51aaaa31
Wallyson <wallyson@aluno.fapce.edu.br>
2022-11-11 21:18:36 -0300

    feature/[MDL02-01] - variables and constants

---
+++ MDL02/variables_and_constantants/variables_and_constantants.py
@@ -0,0 +1,17 @@
+# variable:
+age = 24
+name = "Wally"
+print(f'my name is {name} and i have {age} years.')
+
+age = 25
+name = "Wally"
+print(f'my name is {name} and i have {age} years.')
+
+# python have no const but we can use convention, use a variable with first capital letter:
+ABS_PATH = 'home/etc/bla/bla/bla'
+
+# Good Pratices
+#1 - names use snake_case
+#2 - choose suggestive names
+#3 - Constant names in all uppercase
+
