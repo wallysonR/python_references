38f4b8f7cd5699f516f07bdcb802d1a8d2e35aa7
Wallyson <wallyson@aluno.fapce.edu.br>
2022-11-12 19:18:04 -0300

    feature/[MDL03-05] - operator associate

---
+++ MDL03/operetor/associate/operator.py
@@ -0,0 +1,13 @@
+# Operadores utilizados para verificar se um objeto está presente em uma sequência
+
+curso = "Curso de Python"
+frutas = ["laranja", "uva", "limão"]
+saques = [1500, 100]
+
+operador_in = "Python" in curso
+print(operador_in)
+
+operador_not_in ="maça" not in frutas
+print (operador_not_in)
+
+operador_in = 200 in saques # false pois 200 não está em saques
