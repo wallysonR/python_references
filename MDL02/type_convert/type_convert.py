2fdec06925d29f09e94e09c737c91e0a308b0c2e
Wallyson <wallyson@aluno.fapce.edu.br>
2022-11-11 21:37:26 -0300

    feature/[MDL02-04] -cast types

---
+++ MDL02/type_convert/type_convert.py
@@ -0,0 +1,23 @@
+# float cast
+price = 10
+print (price)
+
+price = float(10)
+print(price)
+
+# int cast
+price = 10.30
+print(price)
+price = int(price)
+print(price)
+
+# division int for float
+price = 10
+print(price)
+
+# return a float number in division
+print(price / 2)
+
+# for return integer we need add two //
+
+print(price//2)
