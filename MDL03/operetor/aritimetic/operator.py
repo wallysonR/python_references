3ace8dd59776553971f670990d5ba221f74cb538
Wallyson <wallyson@aluno.fapce.edu.br>
2022-11-12 17:30:37 -0300

    feature/[MDL01-01] - operator aritmect

---
+++ MDL03/operetor/aritimetic/operator.py
@@ -0,0 +1,25 @@
+print(1+1)
+
+print(1-1)
+
+print(1*1)
+
+print(1/1) # return float
+print(1//1) # return integer
+
+print(10%3) # Módulo
+print(2 ** 3) # Exponenciação
+
+# Precedencia :
+# 1 - parentesis
+# 2 - Expoêntes
+# 3 - Multiplicações e Divisões (da esquerda para direita) 
+# 4 - Somas e subtrações (da esquerda para direita)
+#
+
+print(10 - 5 * 2) # resultado é 0
+print((10-5) * 2) # resultado é 10
+print(10**2 * 2) # resultado é 200
+print (10 ** (2*2)) # resultado é 10000
+# desempate de regra é da esquerda para direita
+print(10/2*4) #resultado é 20.0
