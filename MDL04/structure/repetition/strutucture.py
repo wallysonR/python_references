5c1b71500b2535f828fb9c92333373e302c7fd14
Wallyson <wallyson@aluno.fapce.edu.br>
2022-11-14 19:04:36 -0300

    feature/[MDL04-02] - structure repetition part 2

--- MDL04/structure/repetition/strutucture.py
+++ MDL04/structure/repetition/strutucture.py
@@ -2,39 +2,53 @@
 
 # For - faz sentido usar um for quando quisermos saber o numero de iteração ou quando queremos percorrer um objeto iteravel;
 
-texto = input("Informe um texto: ")
-VOGAIS = "AEIOU"
+# texto = input("Informe um texto: ")
+# VOGAIS = "AEIOU"
 
-for letra in texto:
-    if letra.upper() in VOGAIS:
-        print(letra, end="")
+# for letra in texto:
+#     if letra.upper() in VOGAIS:
+#         print(letra, end="")
 
-print() #quebra a linha      
+# print() #quebra a linha      
 
 # Ranger - Delimita um intervalo de valores é 
 # uma função Built-nt
 # inicio é inclusivo e fim é exclusico
 # ranger(stop) -. ranger object
 # ranger(start, stop[, step]) -> ranger object
-range(4)
-list(range(4))
+# range(4)
+# list(range(4))
 
-for numero in range(0,11):
-    if(numero == 10):
-        print(numero)    
-    else:
-        print(numero, end=" ")
+# for numero in range(0,11):
+#     if(numero == 10):
+#         print(numero)    
+#     else:
+#         print(numero, end=" ")
 
-#outro exemplo com steps
-for numero in range(0, 51, 5):
-    print(numero, end=" ")
+# #outro exemplo com steps
+# for numero in range(0, 51, 5):
+#     print(numero, end=" ")
 
 #While: itera até que uma condicao seja atendida,
 #  faz sentido ser usado quando não sabemos quantas vezes precisaremos executar.
-opcao = -1
-while opcao != 0:
-    opcao = int(input("[1] sacar \n [2] Extrato \n [0] sair \n:"))
-    if opcao == 1:
-        print("Sacando...")
-    elif opcao == 2:
-        print("Exibindo o extrato ...")
+
+# opcao = -1
+# while opcao != 0:
+#     opcao = int(input("[1] sacar \n [2] Extrato \n [0] sair \n:"))
+#     if opcao == 1:
+#         print("Sacando...")
+#     elif opcao == 2:
+#         print("Exibindo o extrato ...")
+
+#Mesma coisa com break
+# while True:
+#     numero = int(input("Digite um numero:"))
+#     if numero == 10:
+#         break
+#     print(numero)
+
+# o continue serve para desviar de algumas situações especificas:
+for numero in range(100):
+    if(numero % 2 == 0):
+        continue
+    print(numero, end=" ")
