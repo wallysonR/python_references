2dfe9cce1772bf530ca544d39d8fd63e66bc972e
Wallyson <wallyson@aluno.fapce.edu.br>
2022-11-16 18:55:33 -0300

    feature/[MDL05-04] - String multilines

---
+++ MDL05/string/multilines_string.py
@@ -0,0 +1,10 @@
+# são defininas informando tres aspas simples ou duplas durante a atribuicao.
+# Elas podem ocupar várias linhas do código e todos os espaços em branco são incluidos na string final
+
+nome = "Wallyson"
+mensagem =f"""
+Olá meu nome é {nome},
+eu estou aprendendo python
+    Essa mensagem tem diferentes recursos
+"""
+print(mensagem)
